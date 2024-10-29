84461a793c320812c0460eaa49b9eac495b9b515
bishnubd26 <bishnunath820@gmail.com>
2024-10-29 06:21:01 +0600

    different ways to handle events in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,25 @@ import './App.css'
 
 function App() {
 
+  function handleClick() {
+    alert('btn clicked');
+  }
+
+  const handleClick2 = () => {
+    alert('btn 2 clicked');
+  }
+
+  const addToFive = num => {
+    alert(num + 5);
+  }
+
   return (
     <>
       <h3>React Core Concepts 2</h3>
-      <button>Click me</button>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={()=>{alert('third clicked')}}>Third</button>
+      <button onClick={()=>addToFive(5)}>add to five</button>
     </>
   )
 }
